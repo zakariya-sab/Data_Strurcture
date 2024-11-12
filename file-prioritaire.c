903812a67acdc7ea14ca576a86d523f710c45edf
zakariya <zakariasabri2022@gmail.com>
2024-11-12 14:10:38 +0100

    code insertion dans une  file prioritaire

---
+++ file-prioritaire.c
@@ -0,0 +1,56 @@
+// file a priorite
+//code insertion dans une  file prioritaire
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node
+{
+    int data;
+    struct node *next;
+    int priorite;
+} node;
+node *rear = NULL;
+node *front = NULL;
+node *cree(int data, int prio)
+{
+    node *new = malloc(sizeof(node));
+    new->data = data;
+    new->priorite = prio;
+    new->next = NULL;
+    return new;
+}
+void push(int data, int prio)
+{
+    // 1er cas : le cas vide
+    if (front == NULL)
+    {
+        front = rear = cree(data, prio);
+    }
+    // 2eme cas : inserstion au debut
+    else if (front->priorite > prio)
+    {
+        node *ptr = cree(data, prio);
+        ptr->next = front;
+        front = ptr;
+    }
+    // 3eme cas : insertion au milieu
+    else if (rear->priorite > prio)
+    {
+        node *ptr = front;
+        node *ptr2 = front;
+        while (ptr->priorite < prio)
+        {
+            ptr2 = ptr;
+            ptr = ptr->next;
+        }
+        node *new = cree(data, prio);
+        new->next = ptr;
+        ptr2->next = new;
+    }
+    //la 4eme cas : insertion au fin 
+    else
+    {
+        node *new = cree(data, prio);
+        rear->next = new;
+        rear = new;
+    }
+}
